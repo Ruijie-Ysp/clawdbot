6f0796de7e7b16ad33b61e905d74537271b025e1
18616211350 <yang_sp@163.com>
2026-04-01 19:43:22 +0800

    gateway: import legacy web-search resolver for pricing bootstrap

--- src/gateway/model-pricing-cache.ts
+++ src/gateway/model-pricing-cache.ts
@@ -8,6 +8,7 @@ import {
   type ModelRef,
 } from "../agents/model-selection.js";
 import type { OpenClawConfig } from "../config/config.js";
+import { resolvePluginWebSearchConfig } from "../config/legacy-web-search.js";
 import { createSubsystemLogger } from "../logging/subsystem.js";
 import { normalizeProviderModelIdWithPlugin } from "../plugins/provider-runtime.js";
 import {
